62719e17d96eadf69cf12f4cc29480ef69815560
Prcnsi <ji.dozin@gmail.com>
2021-08-14 23:07:58 +0900

    .

--- Server/index.php
+++ Server/index.php
@@ -14,12 +14,12 @@
 			  <img class="logoPicture" src="logo_2.jpg" alt="콜로니 로고_1" width=280px, height=100px/>
 			</div>
 		  </header>
-		  <contant class="login-container">
+		  <content class="login-container">
 			<input type="text" placeholder = "아이디" name = "id"><br>
 			<input type="password" placeholder = "비밀번호" name = "pw">
 		 	<input type="submit" value="로그인" class="loginBtn" style="font-weight : bold ; color: white;">
 			
-		  </contant>
+		  </content>
 		  <footer>Don't you have ID?  <a href='./Login/sign_up.php'>sign up</a></footer>
 		</div>
 		<script src="js/login.js"></script>
